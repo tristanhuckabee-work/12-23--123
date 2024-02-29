93e3758a3ee2495cf1ecec242ebc134337e635bd
tristan <thuckabee.work@gmail.com>
2024-02-28 18:11:16 -0600

    finished

--- social.js
+++ social.js
@@ -1,6 +1,5 @@
 // Implement the SocialNetwork class here
 class SocialNetwork {
-
   constructor() {
     this.users = {};
     this.follows = {};
@@ -8,27 +7,67 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here 
+    this.currentID++
+    let newUser = {
+      id: this.currentID,
+      name: name
+    }
+
+    this.users[this.currentID] = newUser;
+    this.follows[this.currentID] = new Set();
+
+    return newUser.id;
   }
 
   getUser(userID) {
-    // Your code here 
+    if (this.users[userID]) return this.users[userID];
+    return null;
   }
 
   follow(userID1, userID2) {
-    // Your code here 
+    if (!this.users[userID1] || !this.users[userID2]) return false;
+
+    this.follows[userID1].add(userID2);
+    return true;
   }
 
   getFollows(userID) {
-    // Your code here 
+    if (this.follows[userID]) return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here 
+    let followers = new Set();
+
+    for (let user in this.follows) {
+      if (this.follows[user].has(userID)) {
+        followers.add(+user);
+      }
+    }
+
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
-    // Your code here 
+    let queue = [[userID]];
+    let visit = new Set([userID]);
+    let res = [];
+
+    while (queue.length) {
+      let curr = queue.shift();
+      let last = curr[curr.length - 1];
+
+      if (curr.length - 2 < degrees) {
+        let followed = this.getFollows(last);
+        followed.forEach( user => {
+          if (!visit.has(user)) {
+            visit.add(user);
+            queue.push([...curr, user]);
+          }
+          if (!this.follows[userID].has(user) && user != userID) res.push(user);
+        })
+      }
+    }
+    return res;
   }
 }
 
